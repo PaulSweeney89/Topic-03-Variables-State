962673ce899dc324911bfdae5347258847762498
PaulSweeney89 <g00229879@gmit.ie>
2020-02-05 23:48:09 +0000

    problems 1 - 2 added

---
+++ lab03.01-hello.py
@@ -0,0 +1,4 @@
+#simple code for inputting your name & outputting 'Hello' [your_name]
+
+name = input("Enter your name:")
+print("Hello",name)
---
+++ lab03.2-sub.py
@@ -0,0 +1,8 @@
+#simple program to calc the difference of 2 inputted numbers
+
+x = float(input("Enter value for x:"))
+y = float(input("Enter value for y:"))
+
+z = x - y
+
+print("The difference between", x, "&", y, "= ", z)
